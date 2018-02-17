c2a9e5450366353fd62e24c761f70f3b82fd4660
kz6fittycent <jimmy.tigert@gmail.com>
2018-02-17 16:37:18 -0600

    version 2.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="magic8ball",
-    version="1.0.0",
+    version="2.0.0",
     description="Ask the Magic 8-ball and learn your fortune",
     long_description=open("README.md").read(),
     license="MIT",
